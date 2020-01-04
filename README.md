73f20577b46aeb67cad46875ac048037dd4c1d46
gavin <Haozhe.18@intl.zju.edu.cn>
2020-01-04 16:11:33 +0800

    做了官方数据集的部分预处理，上传了example

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # 基于mxnet的数字识别模型
 
+
+2020.1.4
+官方数据集的预处理计划启动：
+1. 把官方的annotation中装甲板信息提取出来，做成自己的json
+2. 把官方的原图中的装甲板部分分割出来（可能做数字部分的切割）
+3. 把这两部分信息整合成自己的训练集，初步训练模型之后在根据自己的数据集训练
+   
+   部分预处理图片上传到example中
+   ISSUE:
+   官方的装甲板box把两边的灯条放进去了，自己的应该没有，要割掉这一部分
+   官方识别的装甲板有一些全是长灯条，让人头疼
+   DJI ROCO\robomaster_Central China Regional Competition\image_annotation\AllianceVsArtisans_BO2_2_0.xml这个annotation找不到原图文件
+
+2019.12.26
 基于mxnet框架，用了lenet的结构搭的模型。还没处理训练集，没训练
---
+++ refine_dataset_xmltojson.py
@@ -0,0 +1,121 @@
+# %%
+# import pretty_errors
+import xml.etree.ElementTree as ET
+import os
+import json
+import xmltodict
+import cv2
+import numpy
+
+root_path = "/mnt/e/robomaster/DJI ROCO"  # change to your own path
+dump_path = os.path.join(os.path.dirname(root_path), 'mydump')
+regions = [x for x in os.listdir(root_path)
+           if os.path.isdir(os.path.join(root_path, x))]
+if not os.path.exists(dump_path):
+    os.makedirs(dump_path)
+# %%
+
+
+def test():
+    for region in regions:
+        os.chdir(os.path.join(root_path, region))
+        print("working on %s" % region)
+
+        image_path = os.path.join(root_path, region, "image")
+        annotation_path = os.path.join(root_path, region, "image_annotation")
+
+        image_dump_path = os.path.join(dump_path, region, "image")
+        annotation_dump_path = os.path.join(
+            dump_path, region, "image_annotation")
+        if not os.path.exists(image_dump_path):
+            os.makedirs(image_dump_path)
+        if not os.path.exists(annotation_dump_path):
+            os.makedirs(annotation_dump_path)
+
+        # for each annotation file, find the arnor information
+        # and dump as json file
+        # also cut the useful part of image out
+        for file in os.listdir(annotation_path):
+            i = 0
+            print("refining annotation: "+file)
+            with open(os.path.join(annotation_path, file)) as xml_file:
+                xml_str = xml_file.read()
+                ann_dict = xmltodict.parse(xml_str)
+            img_src = cv2.imread(os.path.join(
+                image_path, ann_dict['annotation']['filename']))
+            # cv2.imshow('2',img_src)
+            for object_ in ann_dict['annotation']["object"]:
+                i += 1
+                if object_['name'] == 'armor':
+                    # collect useful information
+                    for key in object_.keys():
+                        print(key, object_[key])
+                    
+                    # dump to json
+
+                    # cut image (corespondedly)
+                    ymin = int(float(object_['bndbox']['ymin']))
+                    ymax = int(float(object_['bndbox']['ymax']))
+                    xmin = int(float(object_['bndbox']['xmin']))
+                    xmax = int(float(object_['bndbox']['xmax']))
+                    img = img_src[ymin:ymax, xmin:xmax]
+                    cv2.imwrite(os.path.join(image_dump_path,
+                                             os.path.splitext(ann_dict['annotation']['filename'])[0] +"_%s" %i +".jpg"),
+                                img)
+        break
+    return
+
+
+if __name__ == "__main__":
+    # main()
+    test()
+
+
+# %%
+'''
+xml to dict后的结构
+OrderedDict(
+    [
+        ('annotation', OrderedDict([
+            ('filename', 'CUBOTVsNEXT E_BO2_2_47.jpg'),
+            ('version', 'v3.0'),
+            ('review_status', 'passed'),
+            ('time_elapsed', '139'),
+            ('img_label', None),
+            ('size', OrderedDict(
+                [('width', '1920'),
+                 ('height', '1080')])),
+            ('padding_pxt', OrderedDict(
+                [('padding_x', '76'),
+                 ('padding_y', '43')])),
+            ('object',
+             [OrderedDict(
+                 [('name', 'armor'),
+                  ('armor_class', '3'),
+                  ('armor_color', 'red'),
+                  ('generated', '0'),
+                  ('is_incorrect', '0'),
+                  (''bndbox'',' Ord'eredDict(
+                      [('xmin', '359.983'),
+                       ('ymin', '925.287'),
+                       ('xmax', '391.667'),
+                       ('ymax', '943.03')]))]),
+              OrderedDict(
+                  [('name', 'armor'),
+                   ('armor_class', '4'),
+                   ('armor_color', 'red'),
+                   ('generated', '0'),
+                   ('is_incorrect', '0'),
+                   (''bndbox'',' Ord'eredDict(
+                       [('xmin', '539.952'),
+                        ('ymin', '912.613'),
+                        ('xmax', '574.172'),
+                        ('ymax', '931.624')]))]),
+              OrderedDict([('name', 'armor'), ('armor_class', '4'), ('armor_color', 'red'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '514.604'), ('ymin', '888.533'), ('xmax', '538.685'), ('ymax', '907.543')]))]), OrderedDict([('name', 'armor'), ('armor_class', '1'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1121.68'), ('ymin', '256.104'), ('xmax', '1148.3'), ('ymax', '270.046')]))]), OrderedDict([('name', 'armor'), ('armor_class', '2'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '302.95'), ('ymin', '545.07'), ('xmax', '329.565'), ('ymax', '562.813')]))]), OrderedDict([('name', 'armor'), ('armor_class', '4'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1249.69'), ('ymin', '988.657'), ('xmax', '1271.24'), ('ymax', '1008.93')]))]), OrderedDict([('name', 'armor'), ('armor_class', '4'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1195.19'), ('ymin', '979.785'), ('xmax', '1221.81'), ('ymax', '1000.06')]))]), OrderedDict([('name', 'armor'), ('armor_class', '4'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1196.46'), ('ymin', '953.17'), ('xmax', '1219.27'), ('ymax', '965.843')]))]), OrderedDict([('name', 'armor'), ('armor_class', '6'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1333.34'), ('ymin', '239.628'), ('xmax', '1344.75'), ('ymax', '294.126')]))]), OrderedDict([('name', 'armor'), ('armor_class', '6'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1415.72'), ('ymin', '213.013'), ('xmax', '1429.66'), ('ymax', '256.104')]))]), OrderedDict([('name', 'armor'), ('armor_class', '6'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1441.07'), ('ymin', '223.152'), ('xmax', '1455.01'), ('ymax', '262.441')]))]), OrderedDict([('name', 'armor'), ('armor_class', '6'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1484.16'), ('ymin', '251.035'), ('xmax', '1495.57'), ('ymax', '305.533')]))]), OrderedDict([('name', 'armor'), ('armor_class', '8'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1399.24'), ('ymin', '262.441'), ('xmax', '1434.73'), ('ymax', '277.65')]))]), OrderedDict([('name', 'armor'), ('armor_class', '8'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1403.05'), ('ymin', '196.537'), ('xmax', '1441.07'), ('ymax', '209.211')]))]), OrderedDict([('name', 'armor'), ('armor_class', '7'), ('armor_color', 'grey'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1152.1'), ('ymin', '277.65'), ('xmax', '1181.25'), ('ymax', '292.859')]))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         ]), OrderedDict([('name', 'armor'), ('armor_class', '3'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1768.05'), ('ymin', '731.376'), ('xmax', '1789.6'), ('ymax', '749.12')]))]), OrderedDict([('name', 'armor'), ('armor_class', '3'), ('armor_color', 'blue'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1718.63'), ('ymin', '737.713'), ('xmax', '1750.31'), ('ymax', '756.724')]))]), OrderedDict([('name', 'base'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1327'), ('ymin', '188.933'), ('xmax', '1499.37'), ('ymax', '318.207')]))]), OrderedDict([('name', 'watcher'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1144.5'), ('ymin', '253.57'), ('xmax', '1211.67'), ('ymax', '311.87')]))]), OrderedDict([('name', 'car'), ('difficulty', '1'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '209.163'), ('ymin', '450.015'), ('xmax', '356.18'), ('ymax', '581.824')]))]), OrderedDict([('name', 'car'), ('difficulty', '1'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1106.48'), ('ymin', '201.607'), ('xmax', '1199'), ('ymax', '299.196')]))]), OrderedDict([('name', 'car'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1168.58'), ('ymin', '925.287'), ('xmax', '1282.64'), ('ymax', '1027.95')]))]), OrderedDict([('name', 'car'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '1693.28'), ('ymin', '689.552'), ('xmax', '1802.27'), ('ymax', '773.2')]))]), OrderedDict([('name', 'car'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '500.663'), ('ymin', '855.58'), ('xmax', '600.787'), ('ymax', '944.298')]))]), OrderedDict([('name', 'car'), ('difficulty', '0'), ('generated', '0'), ('is_incorrect', '0'), (''bndbox'',' Ord'eredDict([('xmin', '334.635'), ('ymin', '856.848'), ('xmax', '423.352'), ('ymax', '953.17')]))])])
+        ]
+        )
+        )
+    ]
+)
+'''
